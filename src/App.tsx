29003666d6739656c472ac4c3b92767ce1a8b9e7
Andres <zplaguee@gmail.com>
2020-12-02 22:08:23 -0500

    testing components

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import './App.css';
 function App() {
   const [theme,setTheme] = React.useState(false)
   const toggleDark = () => {setTheme(!theme)}
-
   return (
     <div className="App">
       <header className={`App-header ${theme?'dark':''}`}>
         <p className="bg-blue-200 dark:bg-black  m-4 p-7 text-6xl dark:text-gray-300">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <button onClick={toggleDark}  className="bg-white dark:bg-blue-800 text-gray-900 dark:text-red-50 p-5">dark mode toggle</button>
+        <button onClick={toggleDark} className="bg-white dark:bg-blue-800 text-gray-900 dark:text-red-50 p-5">dark mode toggle</button>
       </header>
     </div>
+
   );
 }
 
---
+++ src/components/test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const test = () => {
+    return (
+        <div>
+           <h1>asdasda</h1> 
+        </div>
+    )
+}
