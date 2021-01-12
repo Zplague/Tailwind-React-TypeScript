cc42f173ca96e0bcad36e942902304a3beddf1fd
Andres <zplaguee@gmail.com>
2021-01-11 22:09:41 -0500

    testing themeContext

--- src/utils/ThemeContex.tsx
+++ src/utils/ThemeContex.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 type ThemeContextType = {
-  theme: string;
-  setTheme?: (value: string) => void;
+  enabledDark: boolean,
+  toggleTheme: (enabled:boolean) => void
 };
 
-const ThemeContextDefault = { theme: '' }
-const ThemeContext = React.createContext<ThemeContextType | undefined>(ThemeContextDefault)
-const ThemeContextUpdate = React.createContext('')
+const ThemeContextDefault:ThemeContextType  = { enabledDark:false, toogleTheme:(enabled) => !enabled }
+const ThemeContext = React.createContext<ThemeContextType>(ThemeContextDefault)
 
-export const ThemeProvider = (children: React.ReactNode) => {
-  const [theme, setThemeDark] = React.useState({ theme: '' })
+export const useTheme = () => useContext(ThemeContext);
 
-  function toggleTheme(): any {
-    setThemeDark({ theme: 'dark' })
+/* export const ThemeProvider = (children: React.ReactNode) => {
+  const [theme, setThemeDark] = React.useState(ThemeContextDefault)
+
+  const  toggleTheme = () => {
+    setThemeDark({enabledDark: !theme.enabledDark})
   }
 
   return (
-    <ThemeContext.Provider value={theme}>
-      <ThemeContextUpdate.Provider value={'dark'}>
+    <ThemeContext.Provider value={{toggleTheme, theme}}>
         {children}
-      </ThemeContextUpdate.Provider>
     </ThemeContext.Provider>
   )
-}
+} */
